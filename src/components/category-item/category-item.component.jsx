53792e185a879c9884d139a591ecf018fad82938
pinnet <danny@dannyarnold.com>
2023-12-07 11:46:55 +0000

    complete category item component

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,13 @@
+import './category-item.styles.scss';
+
+const CategoryItem = ({ id, title, imageUrl }) => (
+    <div className="category-container" key={id}>
+        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+        <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+        </div>
+    </div>
+);
+
+export default CategoryItem;
