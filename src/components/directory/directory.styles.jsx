2494de216ba6099dfb15563bbca4c3cea23695c3
pinnet <danny@dannyarnold.com>
2023-12-10 15:26:05 +0000

    Add padding and margin to DirectoryContainer

--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const DirectoryContainer = styled.div`
+  padding-top: 30px;
+  margin: 0 auto;
   width: 100%;
   display: flex;
   flex-wrap: wrap;
