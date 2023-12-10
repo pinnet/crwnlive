01cc689fe527991e969785a28cbcb4038b817a1c
pinnet <danny@dannyarnold.com>
2023-12-10 13:37:13 +0000

    Refactor product-card component structure

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -6,7 +6,7 @@
  * Author: Danny Arnold
  */
 
-import './product-card.styles.scss';
+import { ProductCartContainer, Footer, Name, Price } from './product-card.styles';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
@@ -16,14 +16,14 @@ const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
     const addToCartHandler = () => { addToCart(product); }   
     return (
-        <div className='product-card-container'>    
-            <img alt={name} className='img' src={imageUrl} />
-            <div className='footer'>
-              <span className='name'>{name}</span>
-              <span className='price'>${price}</span>
-            </div>
+        <ProductCartContainer>    
+            <img alt={name} src={imageUrl} />
+            <Footer>
+              <Name>{name}</Name>
+              <Price>${price}</Price>
+            </Footer>
             <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addToCartHandler} >Add to Cart</Button>
-        </div>
+        </ProductCartContainer>
     );
 }
 
