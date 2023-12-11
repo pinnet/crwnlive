deb000d8675d9f73b1025acffca2d73ac40174c1
pinnet <danny@dannyarnold.com>
2023-12-11 14:23:00 +0000

    Update store.js middleware and composeEnhancer

--- src/store/store.js
+++ src/store/store.js
@@ -18,10 +18,9 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middlewares = [process.env.NODE_ENV === 'development' &&  logger].filter(Boolean);
-
-const composeEnhancers = compose(applyMiddleware( ...middlewares ));
+const middlewares = [process.env.NODE_ENV !== 'production' &&  logger].filter(Boolean);
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers = composeEnhancer(applyMiddleware( ...middlewares ));
 
 export const store = createStore( persistedReducer , undefined, composeEnhancers);
 export const persistor = persistStore(store);
