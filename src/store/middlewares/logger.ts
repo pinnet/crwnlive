b3e68ab965453533c381cd1bcd8325c46ed21f24
pinnet <danny@dannyarnold.com>
2023-12-17 00:58:41 +0000

    Refactor loggerMiddleware in store/middlewares/logger.ts

--- src/store/middlewares/logger.ts
+++ src/store/middlewares/logger.ts
@@ -3,13 +3,12 @@
  * @created Mon Dec 11 2023
  * @copyright Copyright (c) 2023 dannyarnold.com
  * @author Danny Arnold
- *
+ */
 import { Middleware } from 'redux';
 import { RootState } from '../store';
 
 export const loggerMiddleware: Middleware<{}, RootState> =
-    (store) => (next) => (action) => {
-        //if(!action) return;
+    (store) => (next) => (action: any) => {
         if (!action.type) {
             return next(action);
         }
@@ -20,4 +19,3 @@ export const loggerMiddleware: Middleware<{}, RootState> =
         console.log('next state', store.getState());
         return result;
     };
-*/
